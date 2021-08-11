4314cc11287499244bbc65fdffe44399701597af
donno2048 <just4now666666@gmail.com>
2021-08-11 17:54:49 +0300

    add option whether to create app or wallpaper

--- app/src/main/java/com/elisha/mandelbrot/MainActivity.java
+++ app/src/main/java/com/elisha/mandelbrot/MainActivity.java
@@ -13,17 +13,23 @@ import processing.core.PApplet;
 public class MainActivity extends AppCompatActivity {
     private PApplet sketch;
     @Override
+    @SuppressWarnings("ConstantConditions")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FrameLayout frame = new FrameLayout(this);
-        frame.setId(CompatUtils.getUniqueViewId());
-        setContentView(frame, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        sketch = new Sketch();
-        PFragment fragment = new PFragment(sketch);
-        fragment.setView(frame, this);
-        Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
-        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(this, Wallpaper.class));
-        startActivity(intent);
+        boolean wallpaper = true; //TODO: (if you need) change this to make the app show the processing but not ask to make it the wallpaper
+        if (wallpaper) {
+            Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+            intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(this, Wallpaper.class));
+            startActivity(intent);
+        }
+        else {
+            FrameLayout frame = new FrameLayout(this);
+            frame.setId(CompatUtils.getUniqueViewId());
+            setContentView(frame, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            sketch = new Sketch();
+            PFragment fragment = new PFragment(sketch);
+            fragment.setView(frame, this);
+        }
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -37,5 +43,6 @@ public class MainActivity extends AppCompatActivity {
         if (sketch != null) {
             sketch.onNewIntent(intent);
         }
+        System.exit(0);
     }
 }
